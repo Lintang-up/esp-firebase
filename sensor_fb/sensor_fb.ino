57b3809a80d510f78fa00547bf7267acc517362f
Lintang-02 <lintang.lintes@gmail.com>
2024-09-18 22:42:33 +0700

    Update Table

--- sensor_fb/sensor_fb.ino
+++ sensor_fb/sensor_fb.ino
@@ -14,8 +14,16 @@ const char* password = "69696969";
 #include "addons/RTDBHelper.h"
 #define API_KEY "AIzaSyBxDSuka_DTyCaCmiaJC6wK_jzuWo3CFW8"
 #define DATABASE_URL "https://sensor-r35-default-rtdb.firebaseio.com/" 
+int currentNumber = 1;
 unsigned long previousMillis = 0; 
 const long interval = 10000; 
+
+// RT
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200, 60000); // Waktu dalam zona WIB (UTC +7)
+
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -45,7 +53,8 @@ float t;
 
 void setup() {
   Serial.begin(9600);
-// Connect to Wi-Fi
+
+  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -56,33 +65,37 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   Serial.println();
-  
-// Firebase
+
+  // Firebase
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
-  if (Firebase.signUp(&config, &auth, "", "")){
+  if (Firebase.signUp(&config, &auth, "", "")) {
     Serial.println("ok");
     signupOK = true;
-  }
-  else{
+  } else {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
   config.token_status_callback = tokenStatusCallback;
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
-  
-// MQ135
-  mq135.calibrate();  
+
+  // NTP Client setup
+  timeClient.begin();
+  timeClient.update();
+
+  // MQ135
+  mq135.calibrate();
   Serial.print("Ro = ");
   Serial.println(mq135.getRo());
-  
-// Ultrasonic
+
+  // Ultrasonic
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
-  
-// DHT
+
+  // DHT
   dht.begin();
 }
+
 
 void loop() {
   unsigned long currentMillis = millis();
@@ -118,29 +131,78 @@ void suhu() {
   }
 }
 
-void fb(){
+void fb() {
   gas();
   ultra();
   suhu();
-  if (Firebase.ready() && signupOK ) {
-    if (Firebase.RTDB.setFloat(&fbdo, "Sensor/humidity", h) && 
-        Firebase.RTDB.setFloat(&fbdo, "Sensor/temperature", t) &&
-        Firebase.RTDB.setFloat(&fbdo, "Sensor/gasLevel", gs) &&
-        Firebase.RTDB.setFloat(&fbdo, "Sensor/jarak", jarak)) {
-    
-        Serial.print("Humidity: ");
+
+  // Perbarui waktu dari NTP
+  timeClient.update();
+
+  // Dapatkan detail waktu
+  int currentHour = timeClient.getHours();
+  int currentMinute = timeClient.getMinutes();
+  int currentSecond = timeClient.getSeconds();
+
+  // Mendapatkan tanggal, bulan, tahun dari epoch time
+  time_t rawTime = timeClient.getEpochTime();
+  struct tm* timeInfo = localtime(&rawTime);
+  int currentDay = timeInfo->tm_mday;
+  int currentMonth = timeInfo->tm_mon + 1; // Karena bulan dimulai dari 0, jadi tambahkan 1
+  int currentYear = timeInfo->tm_year + 1900; // Tahun dimulai dari 1900, jadi tambahkan 1900
+
+  // Format waktu dan tanggal menjadi string yang lebih informatif
+  String timestamp = String(currentHour) + ":" + String(currentMinute) + ":" + String(currentSecond) + "   " +
+                     String(currentDay) + "-" + String(currentMonth) + "-" + String(currentYear);
+
+  // Cek apakah currentNumber sudah ada di Firebase
+  if (Firebase.RTDB.getInt(&fbdo, "/currentNumber")) {
+    currentNumber = fbdo.intData();
+    Serial.println("Current number retrieved: " + String(currentNumber));
+  } else {
+    // Jika path belum ada, inisialisasi currentNumber dengan 1
+    Serial.println("Path not exist, initializing current number to 1.");
+    currentNumber = 1;
+    if (!Firebase.RTDB.setInt(&fbdo, "/currentNumber", currentNumber)) {
+      Serial.println("FAILED to initialize current number");
+      Serial.println("REASON: " + fbdo.errorReason());
+      return; // Jika gagal, hentikan proses
+    }
+  }
+
+  // Buat path berdasarkan nomor urut
+  String path = "/Sensor/" + String(currentNumber);
+
+  if (Firebase.ready() && signupOK) {
+    if (Firebase.RTDB.setFloat(&fbdo, path + "/humidity", h) && 
+        Firebase.RTDB.setFloat(&fbdo, path + "/temperature", t) &&
+        Firebase.RTDB.setFloat(&fbdo, path + "/gasLevel", gs) &&
+        Firebase.RTDB.setFloat(&fbdo, path + "/jarak", jarak) &&
+        Firebase.RTDB.setString(&fbdo, path + "/timestamp", timestamp)) {
+
+        Serial.print("Data sent to path: ");
+        Serial.println(path);
+        Serial.print("Tanggal: ");
+        Serial.print(timestamp);
+        Serial.print(" |  Humidity: ");
         Serial.print(h);
         Serial.print("% |  Temperature: ");
         Serial.print(t);
         Serial.print("C |  GasLevel: ");
         Serial.print(gs);
-        Serial.print("ppm |  Distance: ");
+        Serial.print("ppm | Jarak: ");
         Serial.print(jarak);
-        Serial.print("cm");
-        Serial.println("");
-    }
+        Serial.println("cm");
+
+        // Tingkatkan nomor urut dan simpan kembali di Firebase
+        currentNumber++;
+        if (!Firebase.RTDB.setInt(&fbdo, "/currentNumber", currentNumber)) {
+          Serial.println("FAILED to update current number");
+          Serial.println("REASON: " + fbdo.errorReason());
+        }
+    } 
     else {
-      Serial.println("FAILED");
+      Serial.println("FAILED to send data");
       Serial.println("REASON: " + fbdo.errorReason());
     }
   }
